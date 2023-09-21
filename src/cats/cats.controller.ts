ccc544673cee5fdfc58d1894198f123965d23120
Leo So <leosohc@gmail.com>
2023-09-22 00:37:31 +0800

    added basic cat controller with dto

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,52 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Query,
+  Body,
+} from '@nestjs/common';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { UpdateCatDto } from './dto/update-cat.dto';
 
 @Controller('cats')
-export class CatsController {}
+export class CatsController {
+  // GET /cats --> []
+  @Get()
+  getCats(@Query('type') type: string) {
+    return [{ type }];
+  }
+
+  // GET /cats/:id --> { ... }
+  @Get(':id')
+  getOneCat(@Param('id') id: string) {
+    return {
+      id,
+    };
+  }
+  // POST /cats
+  @Post()
+  createCat(@Body() createCatDto: CreateCatDto) {
+    return {
+      name: createCatDto.name,
+    };
+  }
+
+  // PUT /cats/:id --> { ... }
+  @Put(':id')
+  updateCat(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto) {
+    return {
+      id,
+      name: updateCatDto.name,
+    };
+  }
+  // DELETE /cats/:id
+  @Delete(':id')
+  removeCat(@Param('id') id: string) {
+    return {
+      id,
+    };
+  }
+}
