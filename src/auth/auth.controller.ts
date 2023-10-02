392fb3890877d4584b626a4044dbd84c6458893d
mikes <mikeshum96@gmail.com>
2023-10-02 15:42:36 +0800

    Add session strategy

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,23 @@
-import { Controller, Get, Post } from '@nestjs/common';
-import { AppService } from 'src/app.service';
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { LocalAuthGuard } from './local-auth.guard';
+import { AuthService } from './auth.service';
+import { AuthenticatedGuard } from './authenticated.guard';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly authService: AuthService) {}
 
   // POST /login
+  @UseGuards(LocalAuthGuard)
   @Post('login')
-  login(): any {
-    return {};
+  login(@Request() req): any {
+    return { message: 'Logged in' };
   }
 
   // GET /protected
+  @UseGuards(AuthenticatedGuard)
   @Get('protected')
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(@Request() req): string {
+    return req.user;
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,11 @@ import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
+import { SessionSerializer } from './session.serializer';
 
 @Module({
-  imports: [UsersModule, PassportModule],
+  imports: [UsersModule, PassportModule.register({ session: true })],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalStrategy, SessionSerializer],
 })
 export class AuthModule {}
